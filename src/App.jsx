091ed27a3772023a3a548452607ce892a1429135
MuhammadAli511 <mohammadali417@outlook.com>
2025-07-21 10:41:33 +0500

    Project Setup

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from "react"
+
+function App() {
+
+  return (
+    <>
+      <h1 className="text-3xl font-bold underline">
+        React + Vite + Tailwind
+      </h1>
+    </>
+  )
+}
+
+export default App
